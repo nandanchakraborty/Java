7cdd47ad4eecbc08504cfbc097c7c7c75b17faaa
nandanchakraborty <nandancsebubt@gmail.com>
2024-07-14 09:23:14 +0600

    search element on singly ll

--- Linked_list/src/serarchElement.java
+++ Linked_list/src/serarchElement.java
@@ -1,24 +1,27 @@
-class Node{
+/**
+ *
+ */
+class Node2 {
     int data;
-    Node next;
+    Node2 next;
 
 
-    Node (int data1,Node next1){
+    Node2(int data1, Node2 next1){
     this.data = data1;
     this.next = next1;
 
     }
 
-    Node(int data1){
+    Node2(int data1){
         this.data= data1;
         this.next = null;
     }
 }
 
 public class serarchElement {
-    public static int checkifPresent(Node head, int desireElement) {
+    public static int checkifPresent(Node2 head, int desireElement) {
 
-        Node temp = head;
+        Node2 temp = head;
         while (temp != null) {
             if (temp.data == desireElement) {
                 return 1;
@@ -33,9 +36,9 @@ public class serarchElement {
     }
     public static void main(String[] args){
         int [] arr ={1,2,3};
-        Node head = new Node (arr[0]);
-        head.next = new Node(arr[1]);
-        head.next.next = new Node (arr[2]);
+        Node2 head = new Node2(arr[0]);
+        head.next = new Node2(arr[1]);
+        head.next.next = new Node2(arr[2]);
     int val = 3;
     System.out.println(checkifPresent(head,val)+" here is the result");
     }
